1105ec8a1a68eaa9d7993f55528d7945a9856be7
IvanMOA <ax3l.imo@gmail.com>
2020-10-10 08:59:26 -0500

    Commit inicial

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Escena
+      class="w-50"
+      :cielo="localizacionActual.cielo"
+      :navegadores="localizacionActual.navegadores"
+      :cielos="cielos"
+      @cambiar="cambiar"
+      :video="localizacionActual.video"
+      :informacion="localizacionActual.informacion"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Escena from "./components/Escena.vue";
+const cielos = ["huella.jpg", "vistafrontal.jpg"];
+
+const escenas = {
+  huella: {
+    cielo: "huella.jpg",
+    navegadores: [{ posicion: "-9 -3 0", hacia: "vistafrontal" }],
+  },
+  vistafrontal: {
+    cielo: "vistafrontal.jpg",
+    navegadores: [{ posicion: "9 -3 -10.5", hacia: "huella" }],
+    whatsapp: { posicion: "4 -3 8" },
+    video: { posicion: "-8 0 -2", link: "youtube.com" },
+    informacion: { posicion: "-8 0 2" },
+  },
+};
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      localizacionActual: escenas.huella,
+      cielos: cielos,
+    };
+  },
+  components: { Escena },
+  methods: {
+    cambiar: function (hacia) {
+      this.localizacionActual = escenas[hacia];
+    },
+  },
+  mounted() {},
+};
 </script>
 
 <style>
